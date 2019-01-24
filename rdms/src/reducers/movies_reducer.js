0b727114bf382ddcd54d953cf33eacac4988e25c
unknown <mabbasidesign@yahoo.com>
2019-01-24 16:17:04 +0330

    add reducer

---
+++ rdms/src/reducers/movies_reducer.js
@@ -0,0 +1,25 @@
+
+// 1- Action returns object
+
+// const action = {
+//     type: "MOVIES_LIST",
+//     payload:[
+//         {id: '1', name: 'Pulp Fiction'},
+//         {id: '2', name: 'True Lies'}
+//     ]
+// }
+
+// 2- Reducer finds a match
+
+const state = 0;
+
+export default function(state ={}, action){
+    switch(action.type){
+        case "MOVIES_LIST":
+            return {...state, movies:action.payload}
+        break;
+
+        default:
+            return state;
+    }
+}
